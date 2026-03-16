cf5f0a14d81855fb6e1913db4e3fc9bbf6653032
Levi <levi.al.portugal@gmail.com>
2026-03-16 20:50:50 -0300

    "I created the task class constructor and started building the menu."

---
+++ TaskManagerProject/UI/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagerProject.UI
+{
+    internal class Menu
+    {
+        public void ShowMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("--Bem vindo ao seu Gerênciador de Tarefas--\n");
+                Console.WriteLine("O que deseja ver?\n");
+                Console.WriteLine("Tarefas - 1");
+                Console.WriteLine("Categorias - 2");
+                int response;
+                try
+                {
+                    response = int.Parse(Console.ReadLine());
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+
+                switch (response)
+                {
+                    case 1: 
+                    default:
+                        Console.WriteLine("Essa opção não existe!");
+                        break;
+                }
+
+                continue;
+            }
+        }
+
+    }
+}
